2bf81ff6e292c627f24ab8df1ed6782a25e9b41c
Jalen Ashanti <jalenmshaw@gmail.com>
2024-10-08 12:37:13 -0400

    Persistence Layer: created data transfer object for Category class

---
+++ FlipBuddy.Persistence/DTO/Category_DTO.cs
@@ -0,0 +1,12 @@
+﻿using FlipBuddy.Domain.Models;
+
+namespace FlipBuddy.Persistence.DTO
+{
+	public class Category_DTO
+	{
+		public int CategoryId { get; set; }
+		public string Name { get; set; }= string.Empty;
+		public string Bio { get; set; } = string.Empty;
+		public Category AsDomainCategory() => new(CategoryId, Name, Bio);
+	}
+}
