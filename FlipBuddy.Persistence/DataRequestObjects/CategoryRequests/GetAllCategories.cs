5c5576f7661f2e7f4c9493cbe217368b56d13ae4
Jalen Ashanti <jalenmshaw@gmail.com>
2024-10-08 13:27:13 -0400

    Persistence Layer: Created data transfer request for GetAllCategories

---
+++ FlipBuddy.Persistence/DataRequestObjects/CategoryRequests/GetAllCategories.cs
@@ -0,0 +1,13 @@
+﻿using FlipBuddy.Domain.Constants;
+using FlipBuddy.Persistence.Abstractions;
+using FlipBuddy.Persistence.DTO;
+
+namespace FlipBuddy.Persistence.DataRequestObjects.CategoryRequests
+{
+	public class GetAllCategories : IDataFetch<Category_DTO>
+	{
+		public object? GetParameters() => null;
+
+		public string GetSql() => $"SELECT * FROM {DatabaseTable.Categories}";		
+	}
+}
