14f8bcfdfaa8db7153157f3ba8558b8ace8c2c33
Jalen Ashanti <jalenmshaw@gmail.com>
2024-10-08 12:30:06 -0400

    Domain Layer: Created Category Model

---
+++ FlipBuddy.Domain/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿namespace FlipBuddy.Domain.Models
+{
+	public class Category
+	{
+		public  Category() { }
+
+		public Category(int categoryId,
+						string name,
+						string bio
+						) 
+		{
+			CategoryId = categoryId;
+			Name = name;
+			Bio = bio;	
+		}
+		public int CategoryId { get; set; }
+		public string? Name { get; set; }
+		public string? Bio { get; set; }
+	}
+}
