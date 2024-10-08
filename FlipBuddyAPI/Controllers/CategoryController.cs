09403f963ab4281a9b0b48738848c12f67cf60d4
Jalen Ashanti <jalenmshaw@gmail.com>
2024-10-08 14:41:54 -0400

    API Layer: Created API Category Controller and end point for GetAllCategories

---
+++ FlipBuddyAPI/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using FlipBuddy.Application.Abstraction;
+using FlipBuddy.Application.Requests.CategoryRequests.GetAllCategoriesList;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlipBuddyAPI.Controllers
+{
+	[ApiController]
+	public class CategoryController : BaseController
+	{
+		public CategoryController(IOrchestrator orchestrator) : base(orchestrator)
+		{
+		}
+
+		[HttpGet("Category/GetAllCategories")]
+		public async Task<GetAllCategoriesResponse> GetAllCategories() => await _orchestrator.GetRequestResponseAsync(new GetAllCategoriesRequest());
+	}
+}
