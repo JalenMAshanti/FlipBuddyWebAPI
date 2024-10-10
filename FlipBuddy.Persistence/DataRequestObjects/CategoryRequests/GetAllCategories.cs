a9ca72da2586d466678573bf82fdca192da79d5d
Jalen Ashanti <jalenmshaw@gmail.com>
2024-10-10 01:43:04 -0400

    Persistence Layer: Update GetSql method to include left join for ProductCount

--- FlipBuddy.Persistence/DataRequestObjects/CategoryRequests/GetAllCategories.cs
+++ FlipBuddy.Persistence/DataRequestObjects/CategoryRequests/GetAllCategories.cs
@@ -8,6 +8,11 @@ namespace FlipBuddy.Persistence.DataRequestObjects.CategoryRequests
 	{
 		public object? GetParameters() => null;
 
-		public string GetSql() => $"SELECT * FROM {DatabaseTable.Categories}";		
+		public string GetSql() => $@"SELECT {DatabaseTable.Categories}.*, COUNT({DatabaseTable.Products}.CategoryId) AS ProductCount	
+									FROM {DatabaseTable.Categories}
+									LEFT JOIN {DatabaseTable.Products} 
+									ON {DatabaseTable.Products}.CategoryId = {DatabaseTable.Categories}.Id
+									GROUP BY {DatabaseTable.Categories}.Id;";
+
 	}
 }
