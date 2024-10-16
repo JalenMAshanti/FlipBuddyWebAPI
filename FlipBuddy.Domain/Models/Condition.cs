86eb57268e5a2f7592bc11fb7c42ec3f6d23ed88
Jalen Ashanti <jalenmshaw@gmail.com>
2024-10-15 22:45:25 -0400

    Domain Layer: Created condition model

---
+++ FlipBuddy.Domain/Models/Condition.cs
@@ -0,0 +1,20 @@
+﻿namespace FlipBuddy.Domain.Models
+{
+	public class Condition
+	{
+		public Condition() { }
+
+		public Condition(int conditionId,
+						string conditionTitle,
+						string description)
+		{
+			ConditionId = conditionId;
+			ConditionTitle = conditionTitle;
+			Description = description;
+		}
+
+		public int ConditionId { get; set; }
+		public string? ConditionTitle { get; set; }
+		public string? Description { get; set; }
+	}
+}
