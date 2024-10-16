1a54434d2f7fc22902b9f6e9b44c7ba2fc7e81dd
Jalen Ashanti <jalenmshaw@gmail.com>
2024-10-15 22:50:46 -0400

    Persistence Layer: Created condition data transfer object

---
+++ FlipBuddy.Persistence/DTO/Conditions_DTO.cs
@@ -0,0 +1,11 @@
+﻿using Org.BouncyCastle.Asn1.Cmp;
+
+namespace FlipBuddy.Persistence.DTO
+{
+	public class Conditions_DTO
+	{
+		public int ConditionId { get; set; }
+		public string ConditionTitle { get; set; } = string.Empty;
+		public string Description { get; set; } = string.Empty;
+	}
+}
