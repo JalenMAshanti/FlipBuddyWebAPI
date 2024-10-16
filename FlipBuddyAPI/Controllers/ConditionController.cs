2eba2ea549e579f06e8db3f4f79216b585aee753
Jalen Ashanti <jalenmshaw@gmail.com>
2024-10-16 00:12:46 -0400

    API Layer: Create controller for Conditions and Endpoint for GetAllConditions Request

---
+++ FlipBuddyAPI/Controllers/ConditionController.cs
@@ -0,0 +1,18 @@
+﻿using FlipBuddy.Application.Abstraction;
+using FlipBuddy.Application.Requests.ConditionRequests.GetAllConditionsList;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlipBuddyAPI.Controllers
+{
+
+	[ApiController]
+	public class ConditionController : BaseController
+	{
+		public ConditionController(IOrchestrator orchestrator) : base(orchestrator)
+		{
+		}
+
+		[HttpGet("Category/GetAllConditions")]
+		public async Task<GetAllConditionsResponse> GetAllConditions() => await _orchestrator.GetRequestResponseAsync(new GetAllConditionsRequest());
+	}
+}
