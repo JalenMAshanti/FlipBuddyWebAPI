93d15de8882d34881b711cb88498f0e4b7cf4ebd
Jalen Ashanti <jalenmshaw@gmail.com>
2024-10-16 00:19:49 -0400

    Persistence Layer: Added AsDomainCondition Method to Condition Data Transfer Object

--- FlipBuddy.Persistence/DTO/Conditions_DTO.cs
+++ FlipBuddy.Persistence/DTO/Conditions_DTO.cs
@@ -1,9 +1,13 @@
-﻿namespace FlipBuddy.Persistence.DTO
+﻿using FlipBuddy.Domain.Models;
+
+namespace FlipBuddy.Persistence.DTO
 {
 	public class Conditions_DTO
 	{
 		public int ConditionId { get; set; }
 		public string ConditionTitle { get; set; } = string.Empty;
 		public string Description { get; set; } = string.Empty;
+
+		public Condition AsDomainCondition() => new Condition(ConditionId, ConditionTitle, Description);
 	}
 }
