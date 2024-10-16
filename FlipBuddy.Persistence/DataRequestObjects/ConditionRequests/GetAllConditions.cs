becff410732c3a62037ad22e8368c8bd194dd6d0
Jalen Ashanti <jalenmshaw@gmail.com>
2024-10-15 23:02:46 -0400

    Persistence Layer: Created data request object for GetAllConditions

---
+++ FlipBuddy.Persistence/DataRequestObjects/ConditionRequests/GetAllConditions.cs
@@ -0,0 +1,13 @@
+﻿using FlipBuddy.Domain.Constants;
+using FlipBuddy.Persistence.Abstractions;
+using FlipBuddy.Persistence.DTO;
+
+namespace FlipBuddy.Persistence.DataRequestObjects.ConditionRequests
+{
+	public class GetAllConditions : IDataFetch<Conditions_DTO>
+	{
+		public object? GetParameters() => null;
+
+		public string GetSql() => $@"SELECT * FROM {DatabaseTable.Conditions}";
+	}
+}
