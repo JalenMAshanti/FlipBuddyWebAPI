1ee99b4f2b6b80d69eff44142788dd6ccd9fa23a
Jalen Ashanti <jalenmshaw@gmail.com>
2024-10-15 23:09:53 -0400

    Persistence Test Layer: Create test for GetAllConditions request object

---
+++ FlipBuddy.Persistence.Tests/DataRequestTests/ConditionTests/GetAllConditionsTest.cs
@@ -0,0 +1,17 @@
+﻿using FlipBuddy.Persistence.DataRequestObjects.ConditionRequests;
+
+namespace FlipBuddy.Persistence.Tests.DataRequestTests.ConditionTests
+{
+	public class GetAllConditionsTest : BaseDataRequestTest
+	{
+		#region Happy Path
+		[Fact]
+		public async Task GetAllConditions_ShouldReturn_NotNull()
+		{
+			var rowAffected = await _dataAccess.FetchAsync(new GetAllConditions());
+
+			Assert.NotNull(rowAffected);
+		}
+		#endregion
+	}
+}
