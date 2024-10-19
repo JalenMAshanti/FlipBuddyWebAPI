b63ff60ae7538852dc9f29555eaa198b85f9daed
Jalen Ashanti <jalenmshaw@gmail.com>
2024-10-19 17:01:49 -0400

    Persistence Layer: Added DateSold property to Products_DTO

--- FlipBuddy.Persistence/DTO/Products_DTO.cs
+++ FlipBuddy.Persistence/DTO/Products_DTO.cs
@@ -1,5 +1,4 @@
-﻿using FlipBuddy.Domain.Models;
-using FlippBuddy.Domain.Models;
+﻿using FlippBuddy.Domain.Models;
 
 namespace FlipBuddy.Persistence.DTO
 {
@@ -9,14 +8,15 @@ namespace FlipBuddy.Persistence.DTO
 		public Guid UserGuid { get; set; }
 		public string Title { get; set; } = string.Empty;
 		public int CategoryId { get; set; }
-		public decimal PurchasedPrice { get; set; } 
+		public decimal PurchasedPrice { get; set; }
 		public decimal SellPrice { get; set; }
 		public string Description { get; set; } = string.Empty;
 		public int Quantity { get; set; }
 		public string Currency { get; set; } = string.Empty;
 		public int ConditionId { get; set; }
-		public string BarCode {  get; set; } = string.Empty;
+		public string BarCode { get; set; } = string.Empty;
+		public string DateSold { get; set; } = string.Empty;
 
-		public Product AsDomainProduct() => new(Guid, UserGuid, Title, CategoryId, PurchasedPrice, SellPrice, Description, Quantity, Currency, ConditionId, BarCode);
+		public Product AsDomainProduct() => new(Guid, UserGuid, Title, CategoryId, PurchasedPrice, SellPrice, Description, Quantity, Currency, ConditionId, BarCode, DateSold);
 	}
 }
