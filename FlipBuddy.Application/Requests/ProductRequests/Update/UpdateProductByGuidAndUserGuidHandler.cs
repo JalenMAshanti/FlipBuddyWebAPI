d9f30e893063f71a4d16347b737d0b7ca6b4108a
Jalen Ashanti <jalenmshaw@gmail.com>
2024-10-22 01:53:38 -0400

    ApplicationLayer: Created Request and Handler for UpdateProductByGuidAndUserGuid

---
+++ FlipBuddy.Application/Requests/ProductRequests/Update/UpdateProductByGuidAndUserGuidHandler.cs
@@ -0,0 +1,47 @@
+﻿using FlipBuddy.Application.BaseObjects.BaseHandlers;
+using FlipBuddy.Domain.Exceptions;
+using FlipBuddy.Domain.Models;
+using FlipBuddy.Persistence.Abstractions;
+using FlipBuddy.Persistence.DataRequestObjects.ProductRequests;
+using FlipBuddy.Persistence.DataRequestObjects.UserRequests;
+using FlippBuddy.Domain.Models;
+
+namespace FlipBuddy.Application.Requests.ProductRequests.Update
+{
+    internal class UpdateProductByGuidAndUserGuidHandler : DataRequestHandler<UpdateProductByGuidAndUserGuidRequest>
+    {
+        public UpdateProductByGuidAndUserGuidHandler(IDataAccess dataAccess) : base(dataAccess)
+        {
+        }
+
+        public override async Task ExecuteRequestAsync(UpdateProductByGuidAndUserGuidRequest request)
+        {
+            var user_DTO = _dataAccess.FetchAsync(new GetUserByGuid(request.Guid));
+
+            if (user_DTO == null)
+            {
+                throw new DoesNotExistException(nameof(User), (request.UserGuid, nameof(request.UserGuid)));
+            }
+
+            var product_DTO = _dataAccess.FetchAsync(new GetProductByGuidAndUserGuid(request.UserGuid, request.Guid));
+
+            if (product_DTO == null)
+            {
+                throw new DoesNotExistException(nameof(Product), (request.Guid, nameof(request.Guid)));
+            }
+
+            await _dataAccess.ExecuteAsync(new UpdateProductByGuidAndUserGuid(  request.Guid, 
+                                                                                request.UserGuid, 
+                                                                                request.Title, 
+                                                                                request.CategoryId, 
+                                                                                request.PurchasedPrice, 
+                                                                                request.SellPrice, 
+                                                                                request.Description, 
+                                                                                request.Quantity, 
+                                                                                request.Currency, 
+                                                                                request.ConditionId, 
+                                                                                request.BarCode, 
+                                                                                request.DateSold));
+        }
+    }
+}
---
+++ FlipBuddy.Application/Requests/ProductRequests/Update/UpdateProductByGuidAndUserGuidRequest.cs
@@ -0,0 +1,61 @@
+﻿using FlipBuddy.Application.Abstraction;
+using FlipBuddy.Domain.Validation;
+using FlipBuddy.Domain.Validation.GuidValidation;
+
+namespace FlipBuddy.Application.Requests.ProductRequests.Update
+{
+    public class UpdateProductByGuidAndUserGuidRequest : IValidatable, IRequest
+    {
+
+        public UpdateProductByGuidAndUserGuidRequest() { }
+
+        public UpdateProductByGuidAndUserGuidRequest(Guid guid,
+                                    Guid userGuid,
+                                    string title,
+                                    int categoryId,
+                                    decimal purchasedPrice,
+                                    decimal sellPrice,
+                                    string description,
+                                    int quantity,
+                                    int conditionId,
+                                    string barCode,
+                                    string dateSold)
+        {
+            Guid = guid;
+            UserGuid = userGuid;
+            Title = title;
+            CategoryId = categoryId;
+            PurchasedPrice = purchasedPrice;
+            SellPrice = sellPrice;
+            Description = description;
+            Quantity = quantity;
+            ConditionId = conditionId;
+            BarCode = barCode;
+            DateSold = dateSold;
+        }
+
+        public Guid Guid { get; set; }
+        public Guid UserGuid { get; set; }
+        public string? Title { get; set; }
+        public int CategoryId { get; set; }
+        public decimal PurchasedPrice { get; set; }
+        public decimal SellPrice { get; set; }
+        public string? Description { get; set; }
+        public int Quantity { get; set; }
+        public string? Currency { get; set; }
+        public int ConditionId { get; set; }
+        public string? BarCode { get; set; }
+        public string? DateSold { get; set; }
+
+
+        public bool IsValid(out Validator validator)
+        {
+            validator = new(
+                new GuidRequiredRule(Guid, nameof(Guid)),
+                new GuidRequiredRule(UserGuid, nameof(UserGuid))
+            );
+
+            return validator.IsPassingAllRules;
+        }
+    }
+}
