73254e6c2696a44d27a298bcbf7d2a94cae04046
Jalen Ashanti <jalenmshaw@gmail.com>
2024-10-22 01:48:38 -0400

    API Layer: Added UpdateProductByGuidAndUserGuid Enpoint to Product Controller

--- FlipBuddyAPI/Controllers/ProductController.cs
+++ FlipBuddyAPI/Controllers/ProductController.cs
@@ -2,26 +2,30 @@
 using FlipBuddy.Application.Requests.ProductRequests.GetByGuidAndUserGuid;
 using FlipBuddy.Application.Requests.ProductRequests.GetByUserGuid;
 using FlipBuddy.Application.Requests.ProductRequests.Insert;
+using FlipBuddy.Application.Requests.ProductRequests.Update;
 using FlipBuddy.Application.Requests.ProductRequests.Upload;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FlipBuddyAPI.Controllers
 {
-	[ApiController]
-	public class ProductController : BaseController
-	{
-		public ProductController(IOrchestrator orchestrator) : base(orchestrator) { }
+    [ApiController]
+    public class ProductController : BaseController
+    {
+        public ProductController(IOrchestrator orchestrator) : base(orchestrator) { }
 
-		[HttpPost("Product/InsertProduct")]
-		public async Task InsertProduct([FromBody] InsertProductRequest request) => await _orchestrator.ExecuteRequestAsync(request);
+        [HttpPost("Product/InsertProduct")]
+        public async Task InsertProduct([FromBody] InsertProductRequest request) => await _orchestrator.ExecuteRequestAsync(request);
 
-		[HttpPost("Product/UploadBarcode")]
-		public async Task UploadBarcode(InsertProductByBarcodeRequest request) => await _orchestrator.ExecuteRequestAsync(request);
+        [HttpPost("Product/UploadBarcode")]
+        public async Task UploadBarcode(InsertProductByBarcodeRequest request) => await _orchestrator.ExecuteRequestAsync(request);
 
-		[HttpGet("Product/GetProductsByUserGuid")]
-		public async Task<GetProductsByUserGuidResponse> GetProductsByUserGuid([FromQuery]GetProductsByUserGuidRequest request) => await _orchestrator.GetRequestResponseAsync(request);
+        [HttpGet("Product/GetProductsByUserGuid")]
+        public async Task<GetProductsByUserGuidResponse> GetProductsByUserGuid([FromQuery] GetProductsByUserGuidRequest request) => await _orchestrator.GetRequestResponseAsync(request);
 
-		[HttpGet("Product/GetProductByGuidAndUserGuid")]
-		public async Task<GetProductByGuidAndUserGuidResponse> GetProductByGuidandUserGuid([FromQuery] GetProductByGuidAndUserGuidRequest request) => await _orchestrator.GetRequestResponseAsync(request);
-	}
+        [HttpGet("Product/GetProductByGuidAndUserGuid")]
+        public async Task<GetProductByGuidAndUserGuidResponse> GetProductByGuidandUserGuid([FromQuery] GetProductByGuidAndUserGuidRequest request) => await _orchestrator.GetRequestResponseAsync(request);
+
+        [HttpPut("Product/UpdateProductByGuidAndUserGuid")]
+        public async Task UpdateProductByGuidAndUserGuid([FromBody] UpdateProductByGuidAndUserGuidRequest request) => await _orchestrator.ExecuteRequestAsync(request);
+    }
 }
