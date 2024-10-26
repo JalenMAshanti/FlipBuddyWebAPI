0a6fb7c307bb55d52c695602399cb98baf46eed9
Jalen Ashanti <jalenmshaw@gmail.com>
2024-10-26 08:55:26 -0400

    Application Layer: Condensed DataRequestHanldlerAPIService into oringal DataRequestHandler and updateed DataRequestResponseHandler

--- FlipBuddy.Application/BaseObjects/BaseHandlers/DataRequestHandler.cs
+++ FlipBuddy.Application/BaseObjects/BaseHandlers/DataRequestHandler.cs
@@ -1,11 +1,26 @@
 ﻿using FlipBuddy.Application.Abstraction;
 using FlipBuddy.Persistence.Abstractions;
+using FlipBuddy.Persistence.Implementation;
 
 namespace FlipBuddy.Application.BaseObjects.BaseHandlers
 {
 	internal abstract class DataRequestHandler<TRequest> : BaseRequestHandler<TRequest> where TRequest : IRequest
 	{
 		protected readonly IDataAccess _dataAccess;
+		protected readonly ExternalAPIService? _apiService;
+		protected readonly EbayAPIService? _ebayApiService;
 		protected DataRequestHandler(IDataAccess dataAccess) => _dataAccess = dataAccess;
+			
+		protected DataRequestHandler(IDataAccess dataAccess, ExternalAPIService apiService)
+		{
+			_dataAccess = dataAccess;
+			_apiService = apiService;
+		}
+
+		protected DataRequestHandler(IDataAccess dataAccess, EbayAPIService ebayApiService)
+		{
+			_dataAccess = dataAccess;
+			_ebayApiService = ebayApiService;
+		}
 	}
 }
--- FlipBuddy.Application/BaseObjects/BaseHandlers/DataRequestResponseHandler.cs
+++ FlipBuddy.Application/BaseObjects/BaseHandlers/DataRequestResponseHandler.cs
@@ -1,11 +1,26 @@
 ﻿using FlipBuddy.Application.Abstraction;
 using FlipBuddy.Persistence.Abstractions;
+using FlipBuddy.Persistence.Implementation;
 
 namespace FlipBuddy.Application.BaseObjects.BaseHandlers
 {
 	internal abstract class DataRequestResponseHandler<TRequest, TResponse> : BaseRequestResponseHandler<TRequest, TResponse> where TRequest : IRequestResponse<TResponse>
 	{
 		protected readonly IDataAccess _dataAccess;
+		protected readonly ExternalAPIService? _apiService;
+		protected readonly EbayAPIService? _ebayAPISerice;
 		protected DataRequestResponseHandler(IDataAccess dataAccess) => _dataAccess = dataAccess;
+
+		protected DataRequestResponseHandler(IDataAccess dataAccess, ExternalAPIService apiService)
+		{
+			_dataAccess = dataAccess;
+			_apiService = apiService;
+		}
+
+		protected DataRequestResponseHandler(IDataAccess dataAccess, EbayAPIService ebayApiService)
+		{
+			_dataAccess = dataAccess;
+			_ebayAPISerice = ebayApiService;
+		}
 	}
 }
