a65594cb750e9b05d00e8d97967cdb6a04ecd11e
Jalen Ashanti <jalenmshaw@gmail.com>
2024-10-27 14:58:49 -0400

    Persistence Layer: Created data transfer objects for ProductSpecifics and ProductSpecificValues

---
+++ FlipBuddy.Persistence/DTO/ProductSpecificValues_DTO.cs
@@ -0,0 +1,13 @@
+﻿using FlipBuddy.Domain.Models;
+
+namespace FlipBuddy.Persistence.DTO
+{
+	public class ProductSpecificValues_DTO
+	{
+		public int ValueId { get; set; }
+		public int SpecificId { get; set; }
+		public string SpecificValue { get; set; } = string.Empty;
+
+		public ProductSpecificValue AsDomainProductSpecificValue() => new ProductSpecificValue(ValueId, SpecificId, SpecificValue);
+	}
+}
---
+++ FlipBuddy.Persistence/DTO/ProductSpecifics_DTO.cs
@@ -0,0 +1,13 @@
+﻿using FlipBuddy.Domain.Models;
+
+namespace FlipBuddy.Persistence.DTO
+{
+	public class ProductSpecifics_DTO
+	{
+		public int SpecificId { get; set; }
+		public Guid ProductGuid { get; set; }
+		public string SpecificName { get; set; } = string.Empty;
+
+		public ProductSpecific AsDomainProductSpecific() => new ProductSpecific(SpecificId, ProductGuid, SpecificName);
+	}
+}
