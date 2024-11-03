ac76f87b26b18aa9c9b324491f5de343a4da866b
Jalen Ashanti <jalenmshaw@gmail.com>
2024-11-02 23:41:54 -0400

    Shared Test Layer: Create constants for test object values

---
+++ FlipBuddy.Tests.Shared/Constants/TestValues.cs
@@ -0,0 +1,14 @@
+﻿namespace FlipBuddy.Tests.Shared.Constants
+{
+    public static class TestValues
+    {
+        public const string UserFirstName = "J)^L-$HM58SE!BPS+H)034T$HA$Q$UX=JSK_";
+
+        public const string ProductTitle = "GM*LGB2KI2BFD(2L)9*IMNN#B7JP07K9AC-N";
+
+        public const string SpecificName = "KW5M@90&8C(DK$YKIR&7K*7NJ-$BF3VXAY!#";
+
+        public const string SpecificValue = "24$&Z8F@UCQR!FLH9%J)^AB$J32U@M^H9XH!";
+
+    }
+}
