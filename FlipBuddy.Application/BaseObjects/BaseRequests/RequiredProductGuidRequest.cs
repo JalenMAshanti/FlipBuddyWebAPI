2e72de5c6f8dedfa2048c0b5bb7836229aff1701
Jalen Ashanti <jalenmshaw@gmail.com>
2024-11-02 23:44:19 -0400

    Application Layer: Create RequiredProductGuidRequest BaseObject

---
+++ FlipBuddy.Application/BaseObjects/BaseRequests/RequiredProductGuidRequest.cs
@@ -0,0 +1,21 @@
+﻿using FlipBuddy.Domain.Validation;
+using FlipBuddy.Domain.Validation.GuidValidation;
+
+namespace FlipBuddy.Application.BaseObjects.BaseRequests
+{
+    public abstract class RequiredProductGuidRequest
+    {
+        public RequiredProductGuidRequest() { }
+        public RequiredProductGuidRequest(Guid productGuid) => ProductGuid = productGuid;
+
+
+        public Guid ProductGuid { get; set; }
+
+        public bool IsValid(out Validator validator)
+        {
+            validator = new(new GuidRequiredRule(ProductGuid, nameof(ProductGuid)));
+
+            return validator.IsPassingAllRules;
+        }
+    }
+}
