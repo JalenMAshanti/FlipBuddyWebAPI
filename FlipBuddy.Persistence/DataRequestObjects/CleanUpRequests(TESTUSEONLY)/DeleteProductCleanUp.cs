c5ba22cd530d1e56affc3be73a9c1cf077ca236f
Jalen Ashanti <jalenmshaw@gmail.com>
2024-11-03 00:11:55 -0400

    Persistence Layer: Creeated DataRequestObjects for DeleteProductCleanUp and DeleteUserCleanUp (For test use only)

---
+++ FlipBuddy.Persistence/DataRequestObjects/CleanUpRequests(TESTUSEONLY)/DeleteProductCleanUp.cs
@@ -0,0 +1,16 @@
+﻿using FlipBuddy.Domain.Constants;
+using FlipBuddy.Persistence.Abstractions;
+
+namespace FlipBuddy.Persistence.DataRequestObjects.CleanUpRequests_TESTUSEONLY_
+{
+    public class DeleteProductCleanUp : IDataExecute
+    {
+        public DeleteProductCleanUp(string title) => Title = title;
+        
+        public string Title { get; set; }
+
+        public object? GetParameters() => this;
+
+        public string GetSql() => $@"DELETE FROM {DatabaseTable.Products} WHERE Title = @Title";
+    }
+}
---
+++ FlipBuddy.Persistence/DataRequestObjects/CleanUpRequests(TESTUSEONLY)/DeleteUsersCleanUp.cs
@@ -0,0 +1,17 @@
+﻿using FlipBuddy.Domain.Constants;
+using FlipBuddy.Persistence.Abstractions;
+using System.Web;
+
+namespace FlipBuddy.Persistence.DataRequestObjects.CleanUpRequests_TESTUSEONLY_
+{
+    public class DeleteUsersCleanUp : IDataExecute
+    {
+        public DeleteUsersCleanUp(string firstName) => Firstname = firstName; 
+
+        public string Firstname { get; set; }
+        
+        public object? GetParameters() => this;
+
+        public string GetSql() => $@"DELETE FROM {DatabaseTable.Users} WHERE FirstName = @Firstname";
+    }
+}
