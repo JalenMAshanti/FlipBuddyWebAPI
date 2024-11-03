54918ccd6e473fc6003541c8725098d5d75323ee
Jalen Ashanti <jalenmshaw@gmail.com>
2024-11-03 00:03:27 -0400

    Application Layer: Added Validator to InsertProductByBarcodeRequest

--- FlipBuddy.Application/Requests/ProductRequests/Upload/InsertProductByBarcodeRequest.cs
+++ FlipBuddy.Application/Requests/ProductRequests/Upload/InsertProductByBarcodeRequest.cs
@@ -1,23 +1,35 @@
 ﻿using FlipBuddy.Application.Abstraction;
+using FlipBuddy.Domain.Validation;
+using FlipBuddy.Domain.Validation.GuidValidation;
 using Microsoft.AspNetCore.Http;
 
 namespace FlipBuddy.Application.Requests.ProductRequests.Upload
 {
-	public class InsertProductByBarcodeRequest : IRequest
-	{
-		public InsertProductByBarcodeRequest() { }
+    public class InsertProductByBarcodeRequest : IValidatable, IRequest
+    {
+        public InsertProductByBarcodeRequest() { }
 
-		public InsertProductByBarcodeRequest(IFormFile image, Guid guid, Guid userGuid)
-		{
-			Image = image;
-			Guid = guid;
-			UserGuid = userGuid;
-		}
+        public InsertProductByBarcodeRequest(IFormFile image, Guid guid, Guid userGuid)
+        {
+            Image = image;
+            Guid = guid;
+            UserGuid = userGuid;
+        }
 
-		public IFormFile? Image { get; set; }	
+        public IFormFile? Image { get; set; }
 
-		public Guid Guid { get; set; }
+        public Guid Guid { get; set; }
 
-		public Guid UserGuid { get; set; }
-	}
+        public Guid UserGuid { get; set; }
+
+        public bool IsValid(out Validator validator)
+        {
+            validator = new(
+                new GuidRequiredRule(Guid, nameof(Guid)),
+                new GuidRequiredRule(UserGuid, nameof(Guid))
+            );
+
+            return validator.IsPassingAllRules;
+        }
+    }
 }
