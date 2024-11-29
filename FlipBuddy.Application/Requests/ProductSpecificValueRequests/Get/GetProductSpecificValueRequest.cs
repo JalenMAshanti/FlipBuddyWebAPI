49e31ec3447b380811527795ea373299c70054f4
Jalen Ashanti <jalenmshaw@gmail.com>
2024-11-28 19:54:35 -0500

    Application Layer: Created Handler, Request and Response for GetProductSpecific

---
+++ FlipBuddy.Application/Requests/ProductSpecificValueRequests/Get/GetProductSpecificValueRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlipBuddy.Application.Requests.ProductSpecificValueRequests.Get
+{
+    internal class GetProductSpecificValueRequest
+    {
+    }
+}
