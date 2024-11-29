1058980ead4cdc2afc87a54c90d8277ade65c2b1
Jalen Ashanti <jalenmshaw@gmail.com>
2024-11-28 19:59:28 -0500

    Persistence Layer: Created ExternalEbayAPIService for handler implementation.

---
+++ FlipBuddy.Persistence/Implementation/ExternalEbayAPIService.cs
@@ -0,0 +1,11 @@
+﻿using FlipBuddy.Persistence.Abstractions;
+
+namespace FlipBuddy.Persistence.Implementation
+{
+	public class ExternalEbayAPIService : EbayAPIService
+	{
+		public ExternalEbayAPIService(ClientFactory clientFactory) : base(clientFactory)
+		{
+		}
+	}
+}
