2d77fa4224a969b529a82320d08e2ea48e260220
Jalen Ashanti <jalenmshaw@gmail.com>
2024-11-28 19:55:46 -0500

    Application Layer: Created Request and Handler for InsertProductSpecific

---
+++ FlipBuddy.Application/Requests/ProductSpecificValueRequests/Insert/InsertProductSpecificValueHandler.cs
@@ -0,0 +1,31 @@
+﻿using FlipBuddy.Application.BaseObjects.BaseHandlers;
+using FlipBuddy.Domain.Exceptions;
+using FlipBuddy.Persistence.Abstractions;
+using FlipBuddy.Persistence.DataRequestObjects.ProductSpecificValueRequests;
+
+namespace FlipBuddy.Application.Requests.ProductSpecificValueRequests.Insert
+{
+    internal class InsertProductSpecificValueHandler : DataRequestHandler<InsertProductSpecificValueRequest>
+    {
+        public InsertProductSpecificValueHandler(IDataAccess dataAccess) : base(dataAccess)
+        {
+        }
+
+        public override async Task ExecuteRequestAsync(InsertProductSpecificValueRequest request)
+        {
+            try 
+            {
+               var rowsAffected = await _dataAccess.ExecuteAsync(new InsertProductSpecificValue(request.SpecificId, request.SpecificValue));
+
+                if (rowsAffected != 0) 
+                {
+                    throw new OperationFailedException();
+                }
+            }
+            catch(DataAccessException ex) 
+            {
+                throw new OperationFailedException();
+            }
+        }
+    }
+}
---
+++ FlipBuddy.Application/Requests/ProductSpecificValueRequests/Insert/InsertProductSpecificValueRequest.cs
@@ -0,0 +1,30 @@
+﻿using FlipBuddy.Application.Abstraction;
+using FlipBuddy.Domain.Constants;
+using FlipBuddy.Domain.Validation;
+using FlipBuddy.Domain.Validation.StringValidation;
+
+namespace FlipBuddy.Application.Requests.ProductSpecificValueRequests.Insert
+{
+    public class InsertProductSpecificValueRequest : IRequest, IValidatable
+    {
+        public InsertProductSpecificValueRequest() { }
+        public InsertProductSpecificValueRequest(int specificId, string specificValue)
+        {
+            SpecificId = specificId;
+            SpecificValue = specificValue;
+        }
+
+        public int SpecificId { get; set; }
+        public string SpecificValue { get; set; } = string.Empty;
+
+        public bool IsValid(out Validator validator)
+        {
+            validator = new(
+
+                new StringLengthLimitRule(SpecificValue, nameof(SpecificValue), MaxLength.SpecificValue)
+            );
+
+            return validator.IsPassingAllRules;
+        }
+    }
+}
