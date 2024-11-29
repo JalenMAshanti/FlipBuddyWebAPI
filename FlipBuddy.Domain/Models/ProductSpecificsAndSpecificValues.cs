dab82512ade16f680c99b4a537f05972a2c951c8
Jalen Ashanti <jalenmshaw@gmail.com>
2024-11-28 20:10:17 -0500

    Domain Layer: Created model for ProductSpecificAndSpecificValues

---
+++ FlipBuddy.Domain/Models/ProductSpecificsAndSpecificValues.cs
@@ -0,0 +1,17 @@
+﻿namespace FlipBuddy.Domain.Models
+{
+	public class ProductSpecificsAndSpecificValues
+	{
+		public ProductSpecificsAndSpecificValues(int specificId, string specificName, List<ProductSpecificValue> values) 
+		{
+			SpecificId = specificId;
+			SpecificName = specificName;
+			Values = values;
+		}
+
+		public int SpecificId { get; set; }
+		public string SpecificName { get; set; } = string.Empty;
+
+		public List<ProductSpecificValue> Values { get; set; }
+	}
+}
