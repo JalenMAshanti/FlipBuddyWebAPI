618438ed6da3da013b3adb6973b25686d2b5d56d
Jalen Ashanti <jalenmshaw@gmail.com>
2024-11-28 19:57:35 -0500

    Application Layer: Created Handler and Request for DeleteProductSpecificValue

---
+++ FlipBuddy.Application/Requests/ProductSpecificValueRequests/Delete/DeleteProductSpecificValueHandler.cs
@@ -0,0 +1,31 @@
+﻿using FlipBuddy.Application.BaseObjects.BaseHandlers;
+using FlipBuddy.Domain.Exceptions;
+using FlipBuddy.Persistence.Abstractions;
+using FlipBuddy.Persistence.DataRequestObjects.ProductSpecificValueRequests;
+
+namespace FlipBuddy.Application.Requests.ProductSpecificValuesRequests.Delete
+{
+    internal class DeleteProductSpecificValueHandler : DataRequestHandler<DeleteProductSpecificValueRequest>
+    {
+        public DeleteProductSpecificValueHandler(IDataAccess dataAccess) : base(dataAccess)
+        {
+        }
+
+        public async override Task ExecuteRequestAsync(DeleteProductSpecificValueRequest request)
+        {
+            try
+            {
+                var rowsAffected = await _dataAccess.ExecuteAsync(new DeleteProductSpecificValue(request.ValueId, request.SpecificId));
+
+                if (rowsAffected != 1) 
+                {
+                    throw new OperationFailedException();
+                } 
+            }
+            catch(DataAccessException ex)
+            {
+                throw new OperationFailedException();
+            }
+        }
+    }
+}
---
+++ FlipBuddy.Application/Requests/ProductSpecificValueRequests/Delete/DeleteProductSpecificValueRequest.cs
@@ -0,0 +1,18 @@
+﻿using FlipBuddy.Application.Abstraction;
+
+namespace FlipBuddy.Application.Requests.ProductSpecificValuesRequests.Delete
+{
+    public class DeleteProductSpecificValueRequest : IRequest
+    {
+        public DeleteProductSpecificValueRequest() { }
+
+        public DeleteProductSpecificValueRequest(int valueId, int specificId)
+        {
+            ValueId = valueId;
+            SpecificId = specificId;
+        }
+
+        public int ValueId { get; set; }
+        public int SpecificId { get; set; }
+    }
+}
