ad7cc835872686a1c916a9d1b9385785c9064393
Jalen Ashanti <jalenmshaw@gmail.com>
2024-12-11 02:34:49 -0500

    Application Layer: Created DeleteProduct Handler and Request

---
+++ FlipBuddy.Application/Requests/ProductRequests/Delete/DeleteProductByProductGuidHandler.cs
@@ -0,0 +1,32 @@
+﻿using FlipBuddy.Application.BaseObjects.BaseHandlers;
+using FlipBuddy.Domain.Exceptions;
+using FlipBuddy.Persistence.Abstractions;
+using FlipBuddy.Persistence.DataRequestObjects.ProductRequests;
+using FlippBuddy.Domain.Models;
+
+namespace FlipBuddy.Application.Requests.ProductRequests.Delete
+{
+    internal class DeleteProductByProductGuidHandler : DataRequestHandler<DeleteProductByProductGuidRequest>
+    {
+        public DeleteProductByProductGuidHandler(IDataAccess dataAccess) : base(dataAccess)
+        {
+        }
+
+        public override async Task ExecuteRequestAsync(DeleteProductByProductGuidRequest request)
+        {
+            try
+            {
+                var rowsAffected = await _dataAccess.ExecuteAsync(new DeleteProductByGuid(request.ProductGuid));
+
+                if (rowsAffected <= 0)
+                {
+                    throw new DoesNotExistException(nameof(Product), (request.ProductGuid, nameof(request.ProductGuid)));
+                }
+            }
+            catch
+            {
+                throw new OperationFailedException();
+            }
+        }
+    }
+}
---
+++ FlipBuddy.Application/Requests/ProductRequests/Delete/DeleteProductByProductGuidRequest.cs
@@ -0,0 +1,12 @@
+﻿using FlipBuddy.Application.Abstraction;
+using FlipBuddy.Application.BaseObjects.BaseRequests;
+
+namespace FlipBuddy.Application.Requests.ProductRequests.Delete
+{
+    public class DeleteProductByProductGuidRequest : RequiredProductGuidRequest, IRequest
+    {
+        public DeleteProductByProductGuidRequest() { }
+
+        public DeleteProductByProductGuidRequest(Guid productGuid) : base(productGuid) { }
+    }
+}
